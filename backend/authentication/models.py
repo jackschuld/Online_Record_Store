f9c1e0810d6158e7c26baadb388008d9ab754844
unknown <schuldjack@gmail.com>
2023-03-08 08:20:19 -0800

    Added is_store boolean to authentication models to determine if a user is a store user

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,5 +1,6 @@
 # from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -13,4 +14,4 @@ class User(AbstractUser):
     '''
     # Example (note import of models above that is commented out)
     # this will add a column to the user table
-    # is_student = models.BooleanField('student status', default=False)
+    is_store = models.BooleanField('store status', default=False)
