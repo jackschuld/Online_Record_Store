7fd5e576d14864aa542c9156de2e45276fa85b5e
unknown <schuldjack@gmail.com>
2023-03-08 10:07:26 -0800

    Updated navbar and header of page + added bootstrap links to create a dropdown to view profile info

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -7,21 +7,35 @@ import "./NavBar.css";
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
+  
   return (
     <div className="navBar">
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
+            <a><b>Record Store</b> on Line Ave.</a>
           </Link>
         </li>
         <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Profile
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">{user.username}</a>
+              <a class="dropdown-item" href="#">Collection</a>
+              <a class="dropdown-item" href="#">Wishlist</a>
+              <a class="dropdown-item" href="#">
+                {user ? (
+                  <a onClick={logoutUser}>Logout</a>
+                ) : (
+                  <a onClick={() => navigate("/login")}>Login</a>
+                )}
+              </a>
+            </div>
+          </div>
         </li>
+
       </ul>
     </div>
   );
