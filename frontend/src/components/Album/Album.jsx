1f1013ecd2b03c8f319500b23cf6539b384aefb2
unknown <schuldjack@gmail.com>
2023-03-16 13:38:15 -0700

    Created ability to add a review to an album

--- frontend/src/components/Album/Album.jsx
+++ frontend/src/components/Album/Album.jsx
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-// import TrackList from "../TrackList/TrackList";
+import AuthContext from "../../context/AuthContext";
+import useAuth from "../../hooks/useAuth";
+
 
 const Album = () => {
 
+    const [user, token, config] = useAuth();
+    
     const {album_id} = useParams();
-    const [album, setAlbum] = useState([])
-    const [artist, setArtist] = useState('')
-    const src = `https://open.spotify.com/embed?uri=${album.uri}`
+    const [album, setAlbum] = useState([]);
+    const [artist, setArtist] = useState('');
+    const [review, setReview] = useState('');
+    const [rating, setRating] = useState(0);
+    const src = `https://open.spotify.com/embed?uri=${album.uri}`;
 
 
     useEffect(() => {
@@ -22,10 +28,37 @@ const Album = () => {
             });
             setAlbum(response.data)
             setArtist(response.data.artists[0].name)
+            console.log(response.data)
         }
         fetchAlbum();
-        console.log(album)
     }, []);
+
+
+    async function addAlbumToCollection(newReview){
+        if (localStorage.getItem("token")){
+            let url = "http://127.0.0.1:8000/api/reviews/" + album_id + "/";
+            console.log(config)
+            console.log(url)
+            let response = await axios.post(url, newReview, config);
+            console.log(response);
+        }
+        else {
+            alert('Must be signed in!');
+        }
+    }
+
+    function handleNewReview(event) {
+        if (localStorage.getItem("token")){
+            event.preventDefault();
+            let newReview = {
+                // user_id: user.id,
+                star_review: parseInt(rating),
+                written_review: review,
+            };
+            console.log(newReview)
+            addAlbumToCollection(newReview);
+        }
+    }
 
     return ( 
         <div>
@@ -41,9 +74,17 @@ const Album = () => {
                 Number of Tracks: {album.total_tracks}
                 <br/>
                 Label: {album.label}
+                <br/>
+                <form onSubmit={handleNewReview}>
+                    <label>Review</label>
+                    <div>
+                        <input style={{width: '150px'}} type="number" value={rating} onChange={(event) => setRating(event.target.value)}/>
+                        <input placeholder="Leave the album a review!" value={review} onChange={(event) => setReview(event.target.value)}/>
+                        <button type='submit'>Favorite</button>
+                    </div>
+                </form>
             </p>
             <iframe src={src} width="100%" height="800" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-            {/* <TrackList album_id={album_id}/> */}
         </div>
      );
 }
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import SearchBar from "../SearchPage/SearchPage";
 import "./NavBar.css";
 
 const Navbar = () => {
